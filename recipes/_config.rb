8a1b02f99aa991bdd2738c8df7ab6edc215a134b
Tim Smith <tsmith@llnw.com>
2013-09-07 11:39:21 -0700

    Avoid double quotes where not necessary

--- recipes/_config.rb
+++ recipes/_config.rb
@@ -4,12 +4,12 @@ template "#{node['elasticsearch']['configdir']}/in.sh" do
   owner node['elasticsearch']['user']
   group node['elasticsearch']['group']
   mode 0755
-  notifies :restart, "service[elasticsearch]"
+  notifies :restart, 'service[elasticsearch]'
 end
 
 template "#{node['elasticsearch']['configdir']}/elasticsearch.yml" do
   owner node['elasticsearch']['user']
   group node['elasticsearch']['group']
   mode 0644
-  notifies :restart, "service[elasticsearch]"
+  notifies :restart, 'service[elasticsearch]'
 end
