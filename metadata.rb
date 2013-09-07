0c845c5db30e0f6d58397fab52d599d2460bbf0e
Tim Smith <tsmith@llnw.com>
2013-09-07 11:27:24 -0700

    Add name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
+name              "elasticsearch"
 maintainer        "Nikolay Sturm"
 maintainer_email  "opscode@erisiandiscord.de"
 license           "Apache 2.0"
-description       "Open source, distributed, RESTful, search engine"
+description       "Install elasticsearch the open source, distributed, RESTful, search engine"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.0.4"
 
