2bf34e897dddfac8e37f0c58b7f19e737f729df9
Tim Smith <tsmith@llnw.com>
2013-09-07 11:28:11 -0700

    Use single quotes where interpolation isn't necessary

--- recipes/stand-alone.rb
+++ recipes/stand-alone.rb
@@ -1,9 +1,9 @@
 # Setup a stand-alone elasticsearch server
 
-include_recipe "elasticsearch::_dependencies"
-include_recipe "elasticsearch::_group"
-include_recipe "elasticsearch::_user"
-include_recipe "elasticsearch::_source"
-include_recipe "elasticsearch::_directories"
-include_recipe "elasticsearch::_runit"
-include_recipe "elasticsearch::_config"
+include_recipe 'elasticsearch::_dependencies'
+include_recipe 'elasticsearch::_group'
+include_recipe 'elasticsearch::_user'
+include_recipe 'elasticsearch::_source'
+include_recipe 'elasticsearch::_directories'
+include_recipe 'elasticsearch::_runit'
+include_recipe 'elasticsearch::_config'
