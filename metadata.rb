a30043b41b724a44018eac3f01c6fa41ec91a7b9
Nikolay Sturm <github@erisiandiscord.de>
2013-09-08 19:58:47 +0200

    bump version after elasticsearch version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "opscode@erisiandiscord.de"
 license           "Apache 2.0"
 description       "Install elasticsearch the open source, distributed, RESTful, search engine"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.4"
+version           "0.0.5"
 
 depends "java"
 depends "runit"
